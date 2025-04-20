31bf3715ab89b9078dbb66b6aa136e0c90204246
Christian Cebrero <christiancebrero@Christians-MacBook-Pro.local>
2025-04-19 17:45:51 -0700

    add changes

--- components/About.tsx
+++ components/About.tsx
@@ -9,17 +9,15 @@ export default function About() {
 			{/* Content Card */}
 			<div className="bg-[var(--color-eggshell)] rounded-3xl shadow-xl max-w-4xl mx-auto px-10 py-12 border border-[var(--color-caramel)]">
 				<p className="text-center text-lg sm:text-xl leading-relaxed tracking-wide">
-					I'm a purpose-driven Software Engineer with a love for building
-					elegant solutions to complex problems. With a solid foundation in
-					Mathematics and Computer Science, I combine logic and creativity to
-					craft meaningful experiences.
+					{
+						"I'm a purpose-driven Software Engineer with a love for building elegant solutions to complex problems. With a solid foundation in Mathematics and Computer Science, I combine logic and creativity to craft meaningful experiences."
+					}
 				</p>
 
 				<p className="text-center text-lg sm:text-xl leading-relaxed tracking-wide mt-6">
-					Whether it's refining system architecture or styling pixel-perfect
-					interfaces, I thrive on challenge and growth. Outside of tech, I play
-					piano, spike volleyballs, and dive into snow-covered slopes or cozy
-					Nintendo Switch sessions 🎮.
+					{
+						"Whether it's refining system architecture or styling pixel-perfect interfaces, I thrive on challenge and growth. Outside of tech, I play piano, spike volleyballs, and dive into snow-covered slopes or cozy Nintendo Switch sessions 🎮."
+					}
 				</p>
 			</div>
 
